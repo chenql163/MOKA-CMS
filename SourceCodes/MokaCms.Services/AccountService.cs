981089e9a28dd61f27edb2e001ee7f616ff00ad8
jkimau <jkim.au@gmail.com>
2013-09-23 20:41:01 +1000

    AccountService.cs and AccountServiceTest.cs has been modified

--- SourceCodes/MokaCms.Services/AccountService.cs
+++ SourceCodes/MokaCms.Services/AccountService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace MokaCms.Services
 {
@@ -19,7 +21,32 @@ namespace MokaCms.Services
 		/// <returns>Returns <c>True</c>, if authenticated; otherwise returns <c>False</c>.</returns>
 		public bool Authenticate(string username, string password)
 		{
-			var authenticated = username == "robin" && password == "robin";
+		    var authenticated = false;
+
+            var doc = XDocument.Load(@"C:\Users\moka\Documents\Visual Studio 2012\Projects\MOKA-CMS\Documents\XML Files\Users-justin.xml");
+
+		    if (doc.Root == null)
+		        throw new Exception("Invalid XML");
+		        //return false;
+
+            // xml 파일 로드하는 또 다른 방법
+		    //var xml = new XmlDocument();
+            //xml.Load(@"C:\Users\moka\Documents\Visual Studio 2012\Projects\MOKA-CMS\Documents\XML Files\Users-justin.xml");
+
+            var user =
+		        doc.Root.Elements("User")
+		            .SingleOrDefault(
+		                p =>
+		                    p.Element("Username").Value.ToLower() == username.ToLower() &&
+		                    p.Element("Password").Value == password);
+            // Root는 Users라는 루트 엘리먼트를 말함. 
+            // Elements 는 User 들을 말함
+
+		    if (user != null)
+		        authenticated = true;
+
+            
+			// var authenticated = username == "robin" && password == "robin";
 
 			return authenticated;
 		}
